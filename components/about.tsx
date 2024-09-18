f1abcf234ca9981dc490f94bc177e5477cfe1ab9
Laiba772 <160595947+Laiba772@users.noreply.github.com>
2024-09-18 20:51:40 +0500

    Add files via upload

---
+++ components/about.tsx
@@ -0,0 +1,8 @@
+export default function About(){
+    return(
+        <>
+        <h1><b>About</b></h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum nesciunt incidunt ipsa aliquam soluta tempora nisi. Possimus qui, dolorem impedit expedita doloremque ipsum sit rem, cumque molestias deserunt quo ducimus.</p>
+        </>
+    )
+}
---
+++ components/header.tsx
@@ -0,0 +1,8 @@
+export default function Header(){
+    return(
+        <>
+        <h1><b>Header</b></h1>
+        <p>Class 2 Assingment of Next.js Quater 2.</p>
+        </>
+    )
+}
