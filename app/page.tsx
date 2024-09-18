ba032a8f481291571e5721d5009698d97f90e6d6
Laiba772 <160595947+Laiba772@users.noreply.github.com>
2024-09-18 20:49:47 +0500

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/header";
+import About from "@/components/about";
+
+export default function Home(){
+  return(
+    <>
+    <h1>This is Home Page.</h1>
+    <Header/>
+    
+    <About/>
+    </>
+  );
+}
